d09037acc7e1ae3863ab8bd7b9701e01d35a5b2c
A1caida <46443450+A1caida@users.noreply.github.com>
2019-01-31 19:56:41 +0400

    Add files via upload

---
+++ for1/for1.cpp
@@ -0,0 +1,26 @@
+﻿// степени, хз зачем
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>nul");
+	int a, b , c=0;
+	cout << "Какое число хотите умножить?\n"; 
+	cin >> a;
+	cout << "Степень?\n";
+	cin >> b;
+	for (c=1; c <= 10; c++) {
+
+		a = a * b;
+		cout << a << ", ";
+	}
+	system("pause>nul");
+	return 0;
+}
+
